3e556a2c99cd23cb75eef6445b29c817b5a5470d
Iorgama Porcely <iorgama.p@gmail.com>
2021-07-26 22:41:04 -0300

    Ajustes na page NotFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,7 @@
 import { Container } from '../../styles/globalStyle';
 import { SVG, Wrapper } from './style';
-import { useTheme } from 'styled-components';
 
 export const NotFound = () => {
-  const Theme = useTheme();
   return (
     <Container>
       <Wrapper>
